2bd8dbd61508e8901929a555980ec92267dbd49c
Suraj Radhakrishnan <surajr1711@gmail.com>
2024-09-17 23:23:45 +0400

    Updated footer to github, portfolio and ref design links

--- src/components/PageFooter/PageFooter.tsx
+++ src/components/PageFooter/PageFooter.tsx
@@ -7,20 +7,20 @@ interface PageFooterProps extends React.ComponentPropsWithoutRef<"footer"> {}
 const PageFooter: React.FC<PageFooterProps> = ({ ...props }) => {
 	return (
 		<footer className="flex gap-4 justify-center align-middle text-gold-verlight" {...props}>
-			<QLink href="#" textStyle="label1">
-				© Copyright
+			<QLink href="https://github.com/surajr1711/qitchen-restaurant" textStyle="label1">
+				Github repo
 			</QLink>
 
 			<Diamond lineLength="none" />
 
 			<QLink href="#" textStyle="label1">
-				Licensing
+				Portfolio
 			</QLink>
 
 			<Diamond lineLength="none" />
 
-			<QLink href="#" textStyle="label1">
-				Styleguide
+			<QLink href="https://qitchen-template.framer.website" textStyle="label1">
+				Reference design
 			</QLink>
 		</footer>
 	);
